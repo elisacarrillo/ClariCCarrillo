1579840e7bcb9169dc3a93dfafb674aa1da60f1e
Elisa Carrillo <elisamc2@illinois.edu>
2023-11-03 12:48:02 -0500

    added touches

--- src/components/ContentCreation.jsx
+++ src/components/ContentCreation.jsx
@@ -16,6 +16,16 @@ import image10 from './images/5.png';
 import image11 from './images/6.png';
 import image12 from './images/7.png';
 import image13 from './images/8.png';
+import image14 from './images/Untitled (1).JPEG'
+import image15 from './images/Queervolution.png'
+import image16 from './images/Portfolio.png'
+import image17 from './images/Portfolio Feedback.png'
+import image18 from './images/Portfolio (1)..png'
+import video1 from './images/Dr. Reece Reel.mp4'
+import video2 from './images/ERG Reel 1 (1)..mp4'
+import video3 from './images/Los Angeles.mp4'
+
+
 
 function ContentCreation() {
   // let navigate = useNavigate();
@@ -34,6 +44,9 @@ function ContentCreation() {
             <img src={image5}/>
             <img src={image6}/>
             <img src={image7}/>
+            <img src={image14}/>
+            <img src={image18}/>
+            <img src={image12}/>
           
         </div>
         <div class="column">
@@ -41,19 +54,27 @@ function ContentCreation() {
             <img src={image2}/>
             <img src={image3}/>
             <img src={image4}/>
+            <img src={image15}/>
+            <video src={video1} controls/>
         </div>
         <div class="column">
             
             <img src={image8}/>
             <img src={image9}/>
             <img src={image10}/>
+            <img src={image16}/>
+            <video src={video2} controls/>
         </div>
         <div class="column">
           
             <img src={image11}/>
-            <img src={image12}/>
+            
             <img src={image13}/>
+            <img src={image17}/>
+            <video src={video3} controls/>
+            
         </div>
+       
     </div>
     
   );
